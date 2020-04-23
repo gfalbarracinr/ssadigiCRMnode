5ba8373572fc8944e33b43a14c382d3c16c926fd
cycarrenol <cycarrenol@unal.edu.co>
2020-04-22 22:50:32 -0500

    agregando contactos no existentes en el CRM

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ let worksheet = workbook.Sheets[first_sheet_name];
 let dataJson = XLSX.utils.sheet_to_json(worksheet)
 //console.log(dataJson[0]["NOMBRE EMPRESA"]);
 //console.log(dataJson[0]["NOMBRE EMPRESA"]);
+//console.log(dataJson);
 
 
 let dataNamesCRM = [];
+let dataContactsEmailCRM = []
 let arrIndexCompaniesInCRM = [];
 const urlPostCompany = "https://api.hubapi.com/companies/v2/companies?hapikey=1c76d9a3-4161-4cc3-a2d3-b30a4c6747b5"
 const urlPostContact = "https://api.hubapi.com/contacts/v1/contact/?hapikey=1c76d9a3-4161-4cc3-a2d3-b30a4c6747b5"
@@ -26,46 +28,71 @@ const urlPostContact = "https://api.hubapi.com/contacts/v1/contact/?hapikey=1c76
 
 
 // get all companies by portal 
-axios.get('https://api.hubapi.com/companies/v2/companies/paged?hapikey=1c76d9a3-4161-4cc3-a2d3-b30a4c6747b5&properties=name')
-  .then(function (response) {
-    // handle success
-    response.data.companies.forEach(element => {
-        dataNamesCRM.push(element.properties.name.value);
-        //console.log(response.data.companies.properties.name.value);
-        
-    });
-    InsertCompany();
-    InsertContact();
-    //console.log(data);
-   })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+async function getCompaniesCRM(){
 
+  const response  = await axios.get('https://api.hubapi.com/companies/v2/companies/paged?hapikey=1c76d9a3-4161-4cc3-a2d3-b30a4c6747b5&properties=name')
+  return response;
+}
 
+async function getContactsCRM(){
 
+  const response  = await axios.get('https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=1c76d9a3-4161-4cc3-a2d3-b30a4c6747b5')
+   return response;
+}
+ 
+
+getCompaniesCRM().then(function (result) {
+  console.log("BAAAAAANG");
   
+   
+  result.data.companies.forEach(element => {
+      dataNamesCRM.push(element.properties.name.value);
+  });
+    
+  InsertCompany();
+    
+ }) .catch(function (error) {
+  // handle error
+  console.log(error);
+})
+//******/ 
 
+getContactsCRM().then(function (result) {
+   
 
+  console.log("BOOOOOOM");
+  
+ //console.log("Estos son los resultados de los contactos: ",result.data.contacts["identity-profiles"][0].identities[0].value);
+ //console.log("ESTE ES EL RESULTADO:",result);
+ 
+ result.data.contacts.forEach(element => {
+  
+  //console.log("Este es el elemento: ",element["identity-profiles"][0].identities[0].value);
+  dataContactsEmailCRM.push(element["identity-profiles"][0].identities[0].value);
+
+});
+
+ InsertContacts();
+      
+ }) .catch(function (error) {
+  // handle error
+  console.log(error);
+})
+ 
+ 
+ 
 function InsertCompany(){
 
- // console.log(dataJson[1]["NOMBRE DEL CONTACTO"]);
- //console.log(dataJson);
-
-
-    
     
     dataJson.forEach(element => {
-    
+
+ 
         let companyNameExcel = element["NOMBRE EMPRESA"];
         
         let  isIncludedCRM   = dataNamesCRM.includes(companyNameExcel);
 
-       
 
         
-
         let dataCompany = {
             "properties": [
               {
@@ -78,22 +105,8 @@ function InsertCompany(){
         
         if(!isIncludedCRM){
 
+            console.log("entra");
             
-            var index = -1;
-            var val = companyNameExcel
-            var filteredObj = dataJson.find(function(item, i){
-              console.log(item.name);
-              if(item["NOMBRE EMPRESA"] === val){
-               
-                
-                index = i;
-                return i;
-              }
-            });
-
-             
-            arrIndexCompaniesInCRM.push(index);
-
 
             axios.post(urlPostCompany, dataCompany)
 
@@ -109,36 +122,56 @@ function InsertCompany(){
     });
 }
 
-function InsertContact(){
 
+function InsertContacts(){
+
+    
   dataJson.forEach(element => {
 
-  console.log("****");
-  // hay que  extraer los elementos del arreglo para poder acceder bien a los contactos
-  //console.log(dataJson[element.]["NOMBRE EMPRESA"]);
-  
-  let dataContacts ={
-    "properties": [
-      {
-        "property": "firstname",
-        "value": "isaac"
+      //console.log("entra datajson");
+
+      let ContactsEmailExcel = element["EMAIL CONTACTO"];
+       
+      //console.log(companyContactsEmailExcel);
+
+      let  isIncludedCRM   = dataContactsEmailCRM.includes(ContactsEmailExcel);
+      
+      console.log(dataContactsEmailCRM);
+      
+      console.log(isIncludedCRM);
+      
+      let dataContacts = {
+          "properties": [
+            {
+              "property": "email",
+              "value": ContactsEmailExcel
+            }
+
+          ]
+        }
+
+               
+        if(!isIncludedCRM){
+
+          console.log("entra");
+          
+
+          axios.post(urlPostContact, dataContacts)
+
+          .then( (response)=>{
+              console.log(response);
+            
+          }).catch((error) =>{
+              console.log(error);
+          });
+          
       }
-   
-    ]
-  }
-
-  axios.post(urlPostContact, dataContacts)
-
-  .then( (response)=>{
-      //console.log(response);
     
-  }).catch((error) =>{
-      console.log(error);
+  
   });
-  
-})
-  
 }
+
+
 
 
 
@@ -151,3 +184,4 @@ app.get('/', function (req, res) {
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!');
 });
+ 
